1f5a234910aadc64594a668b0ed65513eb60562d
Hamsiny <459516309@qq.com>
2022-04-22 14:02:03 +1200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks"/>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
 
@@ -19,6 +19,15 @@ export default {
     return {
       tasks: [],
     };
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+    toggleReminder(id) {
+      console.log(id);
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task);
+    }
   },
   created() {
     this.tasks = [
